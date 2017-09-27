c3924d6a27e580af50b728c3833b9165200b3694
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-09-27 11:54:19 -0700

    Functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,11 @@
 var friends = require('../data/friends.js');
 
 module.exports = function(app){
-  app.get('/api/friends', function(req,res){
+  app.get('/data/friends', function(req,res){
     res.json(friends);
   });
-  app.post('/api/friends', function(req,res){ 
+  app.post('/data/friends', function(req,res){ 
     var userScores = req.body.scores;
-    console.log (req.body.scores);
 
   });
 };
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api"}));
 
-require('./app/routing/apiRoutes.js')(app); 
-require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js'); 
+require('./app/routing/htmlRoutes.js');
 
 app.listen(PORT, function ()
   {
