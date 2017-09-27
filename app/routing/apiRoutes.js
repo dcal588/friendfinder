67430aff0e2c874834bfa07f43723a89d00d0063
dcal588 <calderon.david@gmail.com>
2017-09-26 17:37:06 -0700

    testing pulling data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,9 @@ module.exports = function(app){
   app.get('/api/friends', function(req,res){
     res.json(friends);
   });
+  app.post('/api/friends', function(req,res){ 
+    var userScores = req.body.scores;
+    console.log (req.body.scores);
+
+  });
 };
