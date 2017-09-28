6e5a0a917c9187cb8bb1bc098c574460b6550d29
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-09-28 12:52:39 -0700

    Functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,29 @@
 var friends = require('../data/friends.js');
 
 module.exports = function(app){
-  app.get('/data/friends', function(req,res){
+  app.get('/api/friends', function(req,res){
     res.json(friends);
   });
-  app.post('/data/friends', function(req,res){ 
+  app.post('/api/friends', function(req,res){
+    var bestFriend = 0;    
     var userScores = req.body.scores;
+    var allScores = [];
+    var absoluteDiff = 0;
+
+    for(var i=0; i<friends.length; i++){
+      for(var j=0; j<userScores.length; j++){
+        absoluteDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j])));
+      }
+      allScores.push(absoluteDiff);
+    }
+    for(var i=0; i<allScores.length; i++){
+      if(allScores[i] <= allScores[bestFriend]){
+        bestFriend = i;
+      }
+    }
+    var bfIdentified = friends[bestFriend];
+    res.json(bfIdentified);
+    friends.push(req.body);
 
   });
 };
